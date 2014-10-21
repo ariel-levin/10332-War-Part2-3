82c7ad1af5d58148e89e8cd4f7d71a2c08e775d5
ariel <ariel2011@gmail.com>
2014-10-22 00:43:37 +0300

    - startet servlet (the base)

---
+++ Servlet/src/missiles/DefenseDestructorMissile.java
@@ -0,0 +1,77 @@
+package missiles;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import launchers.EnemyLauncher;
+import utils.Utils;
+import utils.WarStatistics;
+
+
+/** Missile for Plane or Ship **/
+public class DefenseDestructorMissile extends Thread {
+
+	private String id;
+	private String whoLaunchedMeId;
+	private String whoLaunchedMeType;
+	private EnemyLauncher launcherToDestroy;
+	private WarStatistics statistics;
+
+	public DefenseDestructorMissile(String id, EnemyLauncher LauncherToDestroy,
+			String whoLunchedMeId, String whoLaunchedMeType,
+			WarStatistics statistics) {
+
+		this.id = id;
+		this.launcherToDestroy = LauncherToDestroy;
+		this.whoLaunchedMeId = whoLunchedMeId;
+		this.whoLaunchedMeType = whoLaunchedMeType;
+		this.statistics = statistics;
+	}
+
+	public void run() {
+		synchronized (launcherToDestroy) {
+			if (launcherToDestroy.isAlive() && !launcherToDestroy.getIsHidden()
+					&& Utils.randomDestractorSucces()) {
+				// Check if the launcher is hidden or not
+				launcherToDestroy.interrupt();
+			}
+		}// synchronized
+	
+		if(launcherToDestroy.isInterrupted()){
+			fireHitEvent();
+			
+		}else {
+			fireMissEvent();
+		}
+	}
+
+	// Event
+	private void fireHitEvent() {
+//		for (WarEventListener l : allListeners) {
+//			l.defenseHitInterceptionLauncher(whoLaunchedMeId,
+//					whoLaunchedMeType, id, launcherToDestroy.getLauncherId());
+//		}
+
+		
+		// update statistics
+		statistics.increaseNumOfLauncherDestroyed();
+	}
+
+	// Event
+	private void fireMissEvent() {
+//		for (WarEventListener l : allListeners) {
+//			l.defenseMissInterceptionLauncher(whoLaunchedMeId,
+//					whoLaunchedMeType, id, launcherToDestroy.getLauncherId());
+//		}
+		
+	}
+
+//	public void registerListeners(WarEventListener listener) {
+//		allListeners.add(listener);
+//	}
+
+	public String getMissileId() {
+		return id;
+	}
+
+}
